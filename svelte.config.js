ffe5b1f8c7020e223cf6d31484c00046352cad2e
PYChen <mmm198219@gmail.com>
2025-04-02 11:13:20 +0800

    fix config

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: process.argv.includes('dev') ? '' : '/cheerdate'
 		}
 	},
 	extensions: ['.svelte', '.svx']
